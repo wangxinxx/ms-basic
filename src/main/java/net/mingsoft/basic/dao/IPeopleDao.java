de7d30807f755378e583763a8640e93b46a07bb0
ms-dev <dev@mingsoft.net>
2017-07-29 11:28:16 +0800

    代码恢复

---
+++ src/main/java/net/mingsoft/basic/dao/IPeopleDao.java
@@ -0,0 +1,21 @@
+package net.mingsoft.basic.dao;
+
+import com.mingsoft.base.dao.IBaseDao;
+import com.mingsoft.util.*;
+import java.util.*;
+
+import org.springframework.stereotype.Component;
+
+import net.mingsoft.basic.entity.PeopleEntity;
+ 
+/**
+ * 通用用户与信息一对多表持久层
+ * @author 伍晶晶
+ * @version 
+ * 版本号：100<br/>
+ * 创建日期：2017-7-27 14:47:29<br/>
+ * 历史修订：<br/>
+ */
+@Component("IBasicPeopleDao")
+public interface IPeopleDao extends IBaseDao {
+}
